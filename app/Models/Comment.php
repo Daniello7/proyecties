d8608880afa1370ada3abb271e0412727af61992
daniello7 <daniellopezolmos7@gmail.com>
2025-01-26 20:05:43 +0100

    Paso 1, Creados Person y Comment: Modelo, Migración, Factoría  y su relación.

---
+++ app/Models/Comment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Comment extends Model
+{
+    /** @use HasFactory<\Database\Factories\CommentFactory> */
+    use HasFactory;
+
+    public function person(): HasOne
+    {
+        return $this->hasOne(Person::class);
+    }
+
+}
---
+++ app/Models/Person.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Person extends Model
+{
+    /** @use HasFactory<\Database\Factories\PersonFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'last_name',
+        'document_number',
+        'company',
+        'comment'
+    ];
+
+    public function comment(): HasOne
+    {
+        return $this->hasOne(Comment::class);
+    }
+}
